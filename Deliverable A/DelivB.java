60b4878971bdbc117edda3fa50fa6b620ab7d75c
Yahya Mohamed <yahya.mohamed816@gmail.com>
2022-12-17 01:43:57 -0600

    Add files via upload

---
+++ Deliverable A/DelivB.java
@@ -0,0 +1,34 @@
+// Class DelivB does the work for deliverable DelivB of the Prog340
+import java.io.File;
+import java.io.PrintWriter;
+
+public class DelivB {
+
+	File inputFile;
+	File outputFile;
+	PrintWriter output;
+	Graph g;
+
+	public DelivB(File in, Graph gr) {
+		inputFile = in;
+		g = gr;
+
+		// Get output file name.
+		String inputFileName = inputFile.toString();
+		String baseFileName = inputFileName.substring(0, inputFileName.length() - 4); // Strip off ".txt"
+		String outputFileName = baseFileName.concat("_out.txt");
+		outputFile = new File(outputFileName);
+		if (outputFile.exists()) { // For retests
+			outputFile.delete();
+		}
+
+		try {
+			output = new PrintWriter(outputFile);
+		} catch (Exception x) {
+			System.err.format("Exception: %s%n", x);
+			System.exit(0);
+		}
+		System.out.println("DelivB:  To be implemented");
+		output.println("DelivB:  To be implemented");
+	}
+}
